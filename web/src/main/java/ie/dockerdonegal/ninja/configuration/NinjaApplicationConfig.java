b29262c8d79abe49039d34f4533e13e10df1f9a3
Romero Ricardo <r.sousa@cinq.com.br>
2018-06-18 18:38:42 +0100

    Removing profile INT

--- web/src/main/java/ie/dockerdonegal/ninja/configuration/NinjaApplicationConfig.java
+++ web/src/main/java/ie/dockerdonegal/ninja/configuration/NinjaApplicationConfig.java
@@ -11,13 +11,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 
 @Slf4j
-@Profile("int")
 @Configuration
 public class NinjaApplicationConfig {
 
