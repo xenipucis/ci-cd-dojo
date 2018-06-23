364f70e0f22e26d503927a5efdf092e67ad6df2b
Adrian Tiberiu (contractor) <Adrian.Tiberiu@sita.aero>
2018-06-23 21:14:47 +0100

    changes.

--- ci/src/test/java/ie/dockerdonegal/ninja/ci/steps/NinjaSteps.java
+++ ci/src/test/java/ie/dockerdonegal/ninja/ci/steps/NinjaSteps.java
@@ -35,35 +35,35 @@ public class NinjaSteps {
 
     @Given("^The (.+) is responsive$")
     public void givenApplicationIsResponsive(final String applicationURL) {
-        //this.applicationURL = applicationURL;
-        //final String uriString = String.format("%s/ninja/ping", applicationURL);
-        //final String result = restTemplate.getForObject(URI.create(uriString), String.class);
-        //Assert.assertEquals("pong", result);
-        Assert.assertTrue(true);
+        this.applicationURL = applicationURL;
+        final String uriString = String.format("%s/ninja/ping", applicationURL);
+        final String result = restTemplate.getForObject(URI.create(uriString), String.class);
+        Assert.assertEquals("pong", result);
+        //Assert.assertTrue(true);
     }
 
     @When("^adding a (.+) as a new ninja$")
     public void addNewNinja(final String ninjaPayload) {
-        //try {
-        //    final NinjaDTO dto = JSON_MAPPER.readValue(ninjaPayload, NinjaDTO.class);
-        //    final String uriString = String.format("%s/ninja/save", applicationURL);
-        //    this.returnedNinjaDTO = restTemplate.postForObject(URI.create(uriString), dto, NinjaDTO.class);
-        //} catch (IOException e) {
-        //    Assert.fail(e.getMessage());
-        //}
-        Assert.assertTrue(true);
+        try {
+            final NinjaDTO dto = JSON_MAPPER.readValue(ninjaPayload, NinjaDTO.class);
+            final String uriString = String.format("%s/ninja/save", applicationURL);
+            this.returnedNinjaDTO = restTemplate.postForObject(URI.create(uriString), dto, NinjaDTO.class);
+        } catch (IOException e) {
+            Assert.fail(e.getMessage());
+        }
+        //Assert.assertTrue(true);
     }
 
     @Then("^the added ninja should be listed$")
     public void assertNinjaIsListed() {
-        //final String uriString = String.format("%s/ninja/list", applicationURL);
-        //final ResponseEntity<NinjasResponse> ninjasResponseEntity = restTemplate.getForEntity(URI.create(uriString), NinjasResponse.class);
-        //Assert.assertNotNull(ninjasResponseEntity);
-        //final NinjasResponse ninjasResponse = ninjasResponseEntity.getBody();
-        //Assert.assertNotNull(ninjasResponse);
-        //final List<NinjaDTO> listOfNinjas = ninjasResponse.getPayload();
-        //Assert.assertNotNull(listOfNinjas);
-        //Assert.assertTrue(listOfNinjas.contains(this.returnedNinjaDTO));
-        Assert.assertTrue(true);
+        final String uriString = String.format("%s/ninja/list", applicationURL);
+        final ResponseEntity<NinjasResponse> ninjasResponseEntity = restTemplate.getForEntity(URI.create(uriString), NinjasResponse.class);
+        Assert.assertNotNull(ninjasResponseEntity);
+        final NinjasResponse ninjasResponse = ninjasResponseEntity.getBody();
+        Assert.assertNotNull(ninjasResponse);
+        final List<NinjaDTO> listOfNinjas = ninjasResponse.getPayload();
+        Assert.assertNotNull(listOfNinjas);
+        Assert.assertTrue(listOfNinjas.contains(this.returnedNinjaDTO));
+        //Assert.assertTrue(true);
     }
 }
